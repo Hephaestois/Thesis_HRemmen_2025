efe565df003ba6ac630e3920369d013b7763037f
Hazel Remmen <jurian.remmen@gmail.com>
2025-05-10 20:52:38 +0200

    Added constant adveciton. Issue with negative coefficients

--- scripts/PDE_model/FiniteVolumeMethod.py
+++ scripts/PDE_model/FiniteVolumeMethod.py
@@ -9,53 +9,47 @@ start_time = time()
 x_range = [-29, -11]
 y_range = [42, 47]
 end_time = 10 #A lot?
-dx = 0.36 #0.36x0.1. This makes the grid square, but the domain suffers for its non-equal resolution.
+dx = 0.36 # 18/5 x 0.1. This makes the grid square (NxN), but the domain suffers for its non-equal resolution.
 dy = 0.1
-dt = 0.01
-
-
+dt = 0.05
 
 grid = Grid(x_range, y_range, dx ,dy)
-grid.setDiffusionConstants(np.eye(2,2))
+grid.setDiffusionConstants(np.array([[0, 0], [0, 0]]))
+grid.setAdvectionConstant(np.array([0, 1]))
 grid.setTimestep(dt)
 
-grid.precalculateDiffusiveMatrix()
+grid.precalculateDiffusiveMatrix(type="Neumann", direction="Horizontal")
+grid.precalculateDiffusiveMatrix(type="Absorbing", direction="Vertical")
+grid.precalculateAdvectiveMatrix()
 
-A = 1.0
-x0 = (-20)
-y0 = (44.5)
-sigma_x = (-18) / 6  # or any scale you prefer
-sigma_y = (-5) / 6
 
-for i in grid.x_idxs:
-    for j in grid.y_idxs:
-        x,y = grid.itc(i, j)
-        value = A * math.exp(-((x - x0)**2) / (2 * sigma_x**2) - ((y - y0)**2) / (2 * sigma_y**2))
-        grid.setValue(grid.cti(x,y), value)
+## IC: Gaussian
+# A = 1.0
+# x0 = (-20)
+# y0 = (43)
+# sigma_x = (-18) / 6
+# sigma_y = (-5) / 6
 
-matrix = grid.getMatrix()
+# for i in grid.x_idxs:
+#     for j in grid.y_idxs:
+#         x,y = grid.itc(i, j)
+#         value = A * math.exp(-((x - x0)**2) / (2 * sigma_x**2) - ((y - y0)**2) / (2 * sigma_y**2))
+#         grid.setValue(grid.cti(x,y), value)
+## End IC
 
-plt.figure(figsize=[10, 4], dpi=100)
-plt.imshow(matrix.T, origin='lower', extent=[-29, -11, 42, 47], aspect  = dx/dy, vmin=0, vmax=1)
-
-plt.colorbar()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Density Field')
-plt.show()
-
+grid.setValue(grid.cti(-20, 44), 1)
 
 N_steps = int(end_time/dt)
 
-print(grid.getTotalValue())
 for i in range(N_steps):
     progressBar(i, N_steps-1, start_time, comment=grid.getTotalValue())
-    grid.timeStep()
-    
+    grid.timeStep(diffusion=True)
+
+
 matrix = grid.getMatrix()
 
 plt.figure(figsize=[10, 4], dpi=100)
-plt.imshow(matrix.T, origin='lower', extent=[-29, -11, 42, 47], aspect  = dx/dy, vmin=0, vmax=1)
+plt.imshow(matrix, origin='lower', extent=[-29, -11, 42, 47], aspect = dx/dy, vmin=0, vmax=1)
 
 plt.colorbar()
 plt.xlabel('x')
--- scripts/PDE_model/library/structures.py
+++ scripts/PDE_model/library/structures.py
@@ -24,58 +24,130 @@ class Grid:
         self.advectionConstants = np.zeros(2)
     
         # d_xx, d_yy operators. x_shape by y_shape matrixes!
-        self.diffusiveMatrix = np.zeros((self.x_num, self.y_num))
-        self.advectiveMatrix = np.zeros((self.x_num, self.y_num))
+        self.diffusiveMatrix_x = np.zeros((self.x_num, self.y_num))
+        self.diffusiveMatrix_y = np.zeros((self.x_num, self.y_num))
+        self.advectiveMatrix_x = np.zeros((self.x_num, self.y_num))
+        self.advectiveMatrix_y = np.zeros((self.x_num, self.y_num))
+        
+        
+        # For absorbing boundaries
+        self.overflow_top = 0
+        self.overflow_bottom = 0
         
         self.dt = 0
     
     def setDiffusionConstants(self, D):
         self.diffusionConstants = D
-        print(self.diffusionConstants)
+        
+    def setAdvectionConstant(self, A):
+        print(A[0], A[1])
+        self.advectionConstants = A
     
     def setValue(self, pos, value):
         self.u_old[pos] = value
     
-    
     def setTimestep(self, timestep):
         self.dt = timestep
         
-    def precalculateDiffusiveMatrix(self):
+    def precalculateDiffusiveMatrix(self, type, direction):
         '''TODO: Code for handling boundary conditions should go here too!!!'''
-        np.fill_diagonal(self.diffusiveMatrix, -2)
-        np.fill_diagonal(self.diffusiveMatrix[1:], 1)    # sub-diagonal
-        np.fill_diagonal(self.diffusiveMatrix[:,1:], 1)  # super-diagonal
+        D = np.zeros((self.x_num, self.y_num))
+        np.fill_diagonal(D, -2)
+        np.fill_diagonal(D[1:], 1)    # sub-diagonal
+        np.fill_diagonal(D[:,1:], 1)  # super-diagonal
         
-        self.diffusiveMatrix[0,0] = -1
-        self.diffusiveMatrix[-1, -1] = -1
-        print(self.diffusiveMatrix)
+        if type == "Neumann":
+            D[0,0] = -1
+            D[-1, -1] = -1
+        if type == "Absorbing":
+            D[0,0] = 0
+            D[0,1] = 0
+            D[-1,-1] = 0
+            D[-1,-2] = 0
+            
+        if direction == "Horizontal":
+            self.diffusiveMatrix_x = D
+        if direction == "Vertical":
+            self.diffusiveMatrix_y = D
+            
+    def precalculateAdvectiveMatrix(self):
+        Ax = np.zeros((self.x_num, self.x_num))
+        np.fill_diagonal(Ax, -1)
+        np.fill_diagonal(Ax[:,1:], 1)
+        self.advectiveMatrix_x = Ax * self.advectionConstants[0]
         
+        Ay = np.zeros((self.y_num, self.y_num))
+        np.fill_diagonal(Ay, -1)
+        np.fill_diagonal(Ay[:,1:], 1)
+        self.advectiveMatrix_y = Ay * self.advectionConstants[1]
+                
+    def timeStep(self, diffusion=True, advection=True):
+        self.u_new = self.u_old
         
-    
-    
-    def timeStep(self):
-        #Left-mul by operator for d_xx, right mul for d_yy.
-        LHS = self.diffusionConstants[0, 0] * np.matmul(self.diffusiveMatrix, self.u_old)
-        RHS = self.diffusionConstants[1, 1] * np.matmul(self.u_old, self.diffusiveMatrix)
-        self.u_new = self.u_old + self.dt * (LHS + RHS)
+        if diffusion:
+            D_x = self.diffusiveMatrix_x  # diffusion operator matrix with Neumann BCs
+            D_y = self.diffusiveMatrix_y  # diffusion operator matrix with absorbing BCs
+            
+            
+            #Left-mul by operator for d_xx, right mul for d_yy.
+            LHS_diff = self.diffusionConstants[0, 0] * np.matmul(D_x, self.u_old)
+            RHS_diff = self.diffusionConstants[1, 1] * np.matmul(self.u_old, D_y)
+            self.u_new += self.dt * (LHS_diff + RHS_diff)
+        
+        if advection:
+            A_x = self.advectiveMatrix_x
+            A_y = self.advectiveMatrix_y
+            
+            LHS_adv = np.matmul(A_x, self.u_old)
+            RHS_adv = np.matmul(self.u_old, A_y)
+            self.u_new += self.dt * (LHS_adv + RHS_adv)
+            
+            
+        #Absorbing boundaries:
+        self.overflow_top += self.u_new[:, -1].sum()
+        self.overflow_bottom += self.u_new[:, 0].sum()
+        
+        #Remove value at the overflow boundary
+        self.u_new[:, 0] = 0
+        self.u_new[:, -1] = 0
+        
         self.u_old = self.u_new
+        
+        
         
     def getMatrix(self):
         return self.u_old
     
     def itc(self, i, j):
         '''Index (i,j) to coordinate (x,y)'''
-        x = min(self.x_carthesian_range)+i*self.x_stepsize
-        y = min(self.y_carthesian_range)+j*self.y_stepsize
+        # For future hazel: This needs to be backwards! It is the correct way around like this!!! i and j are correct!
+        x = min(self.x_carthesian_range)+j*self.x_stepsize
+        y = min(self.y_carthesian_range)+i*self.y_stepsize
         return (x,y)
     
     def cti(self, x, y):
         '''Coordinate (x,y) to index (i, j)'''
-        i = int(np.searchsorted(self.x_s, x))
-        j = int(np.searchsorted(self.y_s, y))
+        # For future hazel: This needs to be backwards! It is the correct way around like this!!! i and j are correct!
+        j = int(np.searchsorted(self.x_s, x))
+        i = int(np.searchsorted(self.y_s, y))
         return (i, j)
         
     def getTotalValue(self):
         return np.sum(self.u_old)
+    
+    def getOverflowBottom(self):
+        return self.overflow_bottom
+    
+    def getOverflowRatio(self):
+        '''Ratio between bottom overflow and total overflow'''
+        try:
+            return self.overflow_bottom / (self.overflow_bottom+self.overflow_top)
+        except:
+            return None
         
+    def getOverflowTop(self):
+        return self.overflow_top
+    
+    def getOverflow(self):
+        return self.overflow_bottom + self.overflow_top
         
