3ea24b906e5e05b65c3f8cf69a7755bd42463a3d
Hazel Remmen <jurian.remmen@gmail.com>
2025-05-28 18:02:38 +0200

    Create intermediate years

--- automated_run.sh
+++ automated_run.sh
@@ -13,46 +13,25 @@ dx=0.1
 dy=0.1
 dt=0.01
 nperday=2
-
-
-### 2016
-# year=2016
-# offset=0
-
-# python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
-# python "discrete_model/RWinVF-${year}.py" $ndays $nperday
-# python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
-
-
-# ### 2018
-# year=2018
-# offset=360
-
-# python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
-# python "discrete_model/RWinVF-${year}.py" $ndays $nperday
-# python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
-
-# ### 2020
-# year=2020
-
-# python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
-# python "discrete_model/RWinVF-${year}.py" $ndays $nperday
-# python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
-
-### 2022
-year=2022
 offset=360
+
+### 2019
+year=2019
 python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
 python "discrete_model/RWinVF-${year}.py" $ndays $nperday
 python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
 
+### 2021
+year=2021
+python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
+python "discrete_model/RWinVF-${year}.py" $ndays $nperday
+python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
 
-### 2024
-# year=2024
-
-# python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
-# python "discrete_model/RWinVF-${year}.py" $ndays $nperday
-# python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
+### 2023
+year=2023
+python "pde_model/FVM${year}.py" $ndays $dx $dy $dt
+python "discrete_model/RWinVF-${year}.py" $ndays $nperday
+python plotting/combined_plot_video.py $year $ndays $dx $dy $dt $nperday $offset
 
 
 
